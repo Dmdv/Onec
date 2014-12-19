37ce8382f4dd9025a77910c73effc3aec4293b77
Dmdv <dimos-d@yandex.ru>
2014-12-19 03:36:01 +0300

    Create onec json description and unit-test for it.

--- Onec.Tests/Controllers/HomeControllerTest.cs
+++ Onec.Tests/Controllers/HomeControllerTest.cs
@@ -1,10 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using Onec;
 using Onec.Controllers;
 
 namespace Onec.Tests.Controllers
@@ -13,26 +8,13 @@ namespace Onec.Tests.Controllers
 	public class HomeControllerTest
 	{
 		[TestMethod]
-		public void Index()
-		{
-			// Arrange
-			HomeController controller = new HomeController();
-
-			// Act
-			ViewResult result = controller.Index() as ViewResult;
-
-			// Assert
-			Assert.AreEqual("Modify this template to jump-start your ASP.NET MVC application.", result.ViewBag.Message);
-		}
-
-		[TestMethod]
 		public void About()
 		{
 			// Arrange
-			HomeController controller = new HomeController();
+			var controller = new HomeController();
 
 			// Act
-			ViewResult result = controller.About() as ViewResult;
+			var result = controller.About() as ViewResult;
 
 			// Assert
 			Assert.IsNotNull(result);
@@ -42,13 +24,26 @@ namespace Onec.Tests.Controllers
 		public void Contact()
 		{
 			// Arrange
-			HomeController controller = new HomeController();
+			var controller = new HomeController();
 
 			// Act
-			ViewResult result = controller.Contact() as ViewResult;
+			var result = controller.Contact() as ViewResult;
 
 			// Assert
 			Assert.IsNotNull(result);
+		}
+
+		[TestMethod]
+		public void Index()
+		{
+			// Arrange
+			var controller = new HomeController();
+
+			// Act
+			var result = controller.Index() as ViewResult;
+
+			// Assert
+			Assert.AreEqual("Modify this template to jump-start your ASP.NET MVC application.", result.ViewBag.Message);
 		}
 	}
 }
---
+++ Onec.Tests/FormatTests/OnecFormatTests.cs
@@ -0,0 +1,26 @@
+﻿using System.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json.Linq;
+
+namespace Onec.Tests.FormatTests
+{
+	[TestClass]
+	[DeploymentItem(@"ParserTests\1c_format.json")]
+	public class OnecFormatTests
+	{
+		private const string JsonFile = "1c_format.json";
+
+		[TestMethod]
+		public void AssumeFormatContainsSomeFields()
+		{
+			var text = File.ReadAllText(JsonFile);
+
+			dynamic jObject = JObject.Parse(text);
+
+			Assert.IsNotNull(jObject["Документ"]);
+			Assert.IsNotNull(jObject.Документ);
+			Assert.IsNotNull(jObject.Документ.Values);
+			Assert.IsTrue(jObject.Документ.Values[0] == "Платежное поручение");
+		}
+	}
+}
